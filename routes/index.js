21c2a0fb1a5338a2bee151b5668d45db4889af3d
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-02-26 11:21:16 +0800

    add new message

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date(),
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
-  }
+    added: new Date(),
+  },
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
-router.get('/new', function(req,res, next){
-  res.render('form',)
-})
+router.post("/new", function (req, res, next) {
+  console.log(req.body);
+  messages.push({text:req.body.messageText, user:req.body.authorName, added: new Date()})
+  res.redirect("/");
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+
 
 module.exports = router;
