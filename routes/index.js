ad93cb3c5e6535d480fe70412df6b18d74229a26
james bariuad <114486039+jamesBariuad@users.noreply.github.com>
2024-02-26 10:04:17 +0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', function(req,res, next){
+  res.render('form',)
+})
+
+module.exports = router;
